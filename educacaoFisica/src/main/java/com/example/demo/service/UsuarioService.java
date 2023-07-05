4e17f1658572b2d0ebe21ddaaacb4e3ccb2fd2ad
Enzoroge <rogeriolima20042000@yahoo.com.br>
2023-07-05 15:41:05 -0300

    Tratamento de excecao Atualizar

--- educacaoFisica/src/main/java/com/example/demo/service/UsuarioService.java
+++ educacaoFisica/src/main/java/com/example/demo/service/UsuarioService.java
@@ -5,11 +5,17 @@ import java.util.Optional;
 
 import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Usuario;
 import com.example.demo.repositories.UsuarioRepository;
+import com.example.demo.service.exceptions.DatabaseException;
 import com.example.demo.service.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UsuarioService {
@@ -33,13 +39,23 @@ public class UsuarioService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		 usuarioRepository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public Usuario atualizar(Long id, Usuario obj) {
+		try {
 		Usuario entity = usuarioRepository.getReferenceById(id);
 		atualizarData(entity, obj);
 		return usuarioRepository.save(entity);
+	}catch (EntityNotFoundException e) {
+		throw new ResourceNotFoundException(id);
+	}
 	}
 
 	private void atualizarData(Usuario entity, Usuario obj) {
--- educacaoFisica/src/main/java/com/example/demo/service/exceptions/DatabaseException.java
+++ educacaoFisica/src/main/java/com/example/demo/service/exceptions/DatabaseException.java
@@ -1,5 +1,12 @@
 package com.example.demo.service.exceptions;
 
-public class DatabaseException {
+public class DatabaseException extends RuntimeException{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
 
 }
